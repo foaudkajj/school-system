934b20fb02877183f143d4d12a2007820193ae36
Rama Mahjoub <ramamhrama093@gmail.com>
2022-10-14 18:36:49 +0300

    #7

--- migration/20221014150625-modify-student-table.js
+++ migration/20221014150625-modify-student-table.js
@@ -10,4 +10,10 @@ module.exports = class modifyStudentTable20221014150625 {
 
     }
 
+    async down(queryRunner) {
+        await queryRunner.query(
+            `alter table \`student\` drop \`tr_name\` varchar(100) not null,
+            drop \`tr_surname\` varchar(100) not null,
+            drop \`gender\` enum('Male','Female') not null ;`);
+    }
 }
